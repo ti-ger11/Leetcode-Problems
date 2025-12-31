87dfb17427b7f569aedca55c0c6c5fa853eecdba
Rshishabh Yadav <78314267+ti-ger11@users.noreply.github.com>
2025-12-31 16:17:26 +0530

    Time: 0 ms (100%), Space: 9.9 MB (55.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,12 +1,30 @@
+char tolowercase(char ch){
+    if(ch>='a' && ch<='z' || (ch>=0 && ch<=9 ))
+        return ch;
+    else{
+        ch=ch-'A'+'a';
+        return ch;
+    }
+}
+
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string newStr = "";
-        for (char c : s) {
-            if (isalnum(c)) {
-                newStr += tolower(c);
-            }
+        int i=0,j=s.length()-1;
+    while(i<=j){
+        if(isalnum(s[i])==0)
+            i++;
+        else if(isalnum(s[j])==0)
+            j--;
+        else if(tolowercase(s[i])!=tolowercase(s[j]))
+            return false;
+        else{
+            i++;
+            j--;
         }
-        return newStr == string(newStr.rbegin(), newStr.rend());
+    }
+    return true;
+    
     }
 };
