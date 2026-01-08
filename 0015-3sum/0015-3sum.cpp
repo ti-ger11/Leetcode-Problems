f5cca4618b31c70e05877355b98a22d1b3742646
Rshishabh Yadav <78314267+ti-ger11@users.noreply.github.com>
2026-01-08 10:23:21 +0530

    Time: 41 ms (87.31%), Space: 28.8 MB (98.86%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,25 +3,42 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         int n = nums.size();
+        
+        // Sort the array to handle duplicates
         sort(nums.begin(), nums.end());
         
         for(int i = 0; i < n - 2; i++) {
-            // Skip duplicates
+            // Skip duplicate first elements
             if(i > 0 && nums[i] == nums[i - 1])
                 continue;
             
-            unordered_set<int> seen;
-            for(int j = i + 1; j < n; j++) {
-                int complement = -nums[i] - nums[j];
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            
+            while(left < right) {
+                int sum = nums[left] + nums[right];
                 
-                if(seen.count(complement)) {
-                    res.push_back({nums[i], complement, nums[j]});
+                if(sum == target) {
+                    res.push_back({nums[i], nums[left], nums[right]});
                     
-                    // Skip duplicates for nums[j]
-                    while(j + 1 < n && nums[j] == nums[j + 1])
-                        j++;
+                    // Skip duplicates for left pointer
+                    while(left < right && nums[left] == nums[left + 1])
+                        left++;
+                    
+                    // Skip duplicates for right pointer
+                    while(left < right && nums[right] == nums[right - 1])
+                        right--;
+                    
+                    left++;
+                    right--;
                 }
-                seen.insert(nums[j]);
+                else if(sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
         
