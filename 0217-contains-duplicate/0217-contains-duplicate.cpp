a783b2b794f6167f6c9617cd469908bb62f97d4a
Rshishabh Yadav <78314267+ti-ger11@users.noreply.github.com>
2026-04-04 18:03:56 +0530

    Time: 51 ms (49.58%), Space: 91 MB (28.44%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int>s;
+
+        for(auto x:nums)
+        {
+            if(s.count(x))
+            {
+                return true;
+
+            }
+            s.insert(x);
+        }
+
+        return false;
+        
+    }
+};
